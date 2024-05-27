dcddb9b2b9c02d01dafc06ac17fbbf30eed281b6
nahuellch <163903861+nahuellch@users.noreply.github.com>
2024-05-27 15:53:59 -0300

    contraseñaaceptar

---
+++ Assets/scripteada.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class scripteada : MonoBehaviour
+{
+    public Text inputText;
+    public string password;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Aceptar () 
+    { 
+    
+    if (inputText.text == password) // contraseña123 
+        {
+
+            Debug.Log("acces garanted");
+            
+        }else { Debug.Log("acces denied"); }
+    
+    }
+}
